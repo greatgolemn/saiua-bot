767ee66758764b32cf9d0beebd3868b26d880016
greatgolemn <62272997+greatgolemn@users.noreply.github.com>
2025-06-11 14:29:03 +0700

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -48,15 +48,7 @@ async function handleMessage(senderPsid, receivedMessage) {
 }
 
 // ✅ ส่งข้อความกลับไปหา Facebook
-function callSendAPI(senderPsid, response) {
-  const requestBody = {
-    recipient: { id: senderPsid },
-    message: response,
-  };
-
-  const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-
-  const axios = require("axios");
+const axios = require("axios");
 
 function callSendAPI(senderPsid, response) {
   const requestBody = {
