3a08d26187246a1b8e52fcaa89d1365a17ca0fb0
greatgolemn <62272997+greatgolemn@users.noreply.github.com>
2025-06-11 14:27:15 +0700

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -56,22 +56,30 @@ function callSendAPI(senderPsid, response) {
 
   const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-  const axios = require('axios');
-  axios.post(
-    {
-      uri: "https://graph.facebook.com/v17.0/me/messages",
-      qs: { access_token: PAGE_ACCESS_TOKEN },
-      method: "POST",
-      json: requestBody,
-    },
-    (err, res, body) => {
-      if (!err) {
-        console.log("Message sent!");
-      } else {
-        console.error("Unable to send message:" + err);
-      }
-    }
-  );
+  const axios = require("axios");
+
+function callSendAPI(senderPsid, response) {
+  const requestBody = {
+    recipient: { id: senderPsid },
+    message: response,
+  };
+
+  const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+  const url = `https://graph.facebook.com/v17.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`;
+
+  axios
+    .post(url, requestBody, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then(() => {
+      console.log("Message sent!");
+    })
+    .catch((err) => {
+      console.error("Unable to send message:", err.response?.data || err.message);
+    });
 }
 
 module.exports = router;
