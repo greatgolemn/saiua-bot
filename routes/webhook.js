5fefd56084e1901ab80fb30ce10cbe87d836b820
greatgolemn <62272997+greatgolemn@users.noreply.github.com>
2025-06-11 14:21:02 +0700

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -57,7 +57,7 @@ function callSendAPI(senderPsid, response) {
   const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
   const axios = require('axios');
-  request(
+  axios.post(
     {
       uri: "https://graph.facebook.com/v17.0/me/messages",
       qs: { access_token: PAGE_ACCESS_TOKEN },
